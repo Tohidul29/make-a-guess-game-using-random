ca188ae19eeb0a278e727f3ae0d56348f426f1fb
MD TOHIDUL ISLAM <64440287+Tohidul29@users.noreply.github.com>
2020-09-07 23:46:05 +0600

    Create python.py

---
+++ python.py
@@ -0,0 +1,20 @@
+#using import module:
+
+import random   
+winning_number = random.randint(1,10)
+guess = 1
+number = int(input("Guess a number between 1 to 100 : "))
+game_over = False
+while not game_over:
+    if number == winning_number:
+        print(f"You win ! and you guessed this number is {guess} times")
+        game_over = True
+    else:
+        if number < winning_number:
+            print("Too low")
+            guess += 1
+            number = int(input("Please guess again : "))
+        else:
+            print("too high")
+            guess += 1
+            number = int(input("Please guess again : "))
