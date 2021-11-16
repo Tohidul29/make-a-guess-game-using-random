65c6b24889627d7d8fcb9540f4a08d722ad32c72
MD TOHIDUL ISLAM <64440287+Tohidul29@users.noreply.github.com>
2021-11-16 20:30:58 +0600

    Update python.py

--- python.py
+++ python.py
@@ -3,7 +3,7 @@
 import random   
 winning_number = random.randint(1,10)
 guess = 1
-number = int(input("Guess a number between 1 to 100 : "))
+number = int(input("Guess a number between 1 to 10 : "))
 game_over = False
 while not game_over:
     if number == winning_number:
